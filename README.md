322be33a522d1586b8fef558e558c3ee966a04e0
marek <marekpl24@wp.pl>
2022-04-14 12:35:42 +0200

    selecting included

--- README.md
+++ README.md
@@ -5,11 +5,12 @@ You have to install eng_to_ipa
 After copy-paste any English text (replacing buit-in example) simply press the button to receive an answer.
 
 ## Transform possibilities:
-  
-* simple: only IPA text 
-  
-* normal: normal text & IPA text (each line separately) 
 
-* list: as previous, but if different version of a word, there's variants in brackets
-  
+- simple: only IPA text
+- normal: normal text & IPA text (each line separately)
+
+- list: as previous, but if different version of a word, there's variants in brackets
+
 Text example: classic English (American) poetry, you all know who is the autor and which is a title :)
+
+A bar between texts may contain IPA version of selected fragment, if you select one...
--- english.py
+++ english.py
@@ -1,3 +1,4 @@
+from tkinter.font import ITALIC
 import eng_to_ipa
 import tkinter as tk
 import re
@@ -62,26 +63,55 @@ def transform_list(txt):
     res = ' '.join(m)
     return f"{txt}\n{res}\n\n"
 
+
+def alarmed():
+    if not txt1.tag_ranges('sel'):
+        return
+    if tk.SEL_FIRST and tk.SEL_LAST:
+        s = txt1.get(tk.SEL_FIRST, tk.SEL_LAST)
+        print(s)
+    
+def search_word():
+    x = txt1.tag_ranges('sel')
+    if x:
+        s=txt1.get(x[0], x[1])
+        labelIPA['text'] = f"{eng_to_ipa.convert(s)}"
+
+    # print("selected")
+        # alarmed()
+    # root.after(1000, alarmed)
+    # return 'break'
+    # print(txt1.get(*ranges))
+
 root = tk.Tk()
 root.title("IPA reading of English text")
 root.geometry("+5+5")
-txt1 = tk.Text(root, height=8, wrap='word', font=('Times', 16), padx=10)
-txt2 = tk.Text(root, wrap='word',font=('Times', 16), padx=10)
+txt1 = tk.Text(root, height=8, wrap='word', font=('Times', 16), padx=10, pady=10)
+txt2 = tk.Text(root, wrap='word',font=('Times', 16), padx=10, pady=15)
 txt2.tag_configure('bold', font=('Times', 16, 'bold'))
-                       
+
+# txt2.bind('<Double-Button-1>', lambda: print('jajko'))
+# txt1.bind('<Button-1>', lambda ev: search_word())
+# txt1.bind('<Double-Button-1>', lambda ev: search_word())
+txt1.bind('<<Selection>>', lambda ev: search_word())
+# txt1.bind('<KeyPress>', lambda ev: search_word())
     
-btnExit = tk.Button(root,text="Exit", command=sys.exit)
+frame = tk.Frame(root)
+btnExit = tk.Button(frame,text="Exit", command=sys.exit)
+frame.grid(row=0,column=1, sticky='nwe')
+btnExit.pack(fill=tk.X)
 frame = tk.Frame(root)
 btn1 = tk.Button(frame,text="Transform simple", command=transform_simple)
 btn2 = tk.Button(frame,text="Transform normal", command=transform_normal)
 btn3 = tk.Button(frame,text="Transform list", command=transform_list)
 txt1.grid(row=0,column=0)
-txt2.grid(row=1,column=0, sticky='we')
-btnExit.grid(row=0,column=1, sticky='nwe')
-frame.grid(row=1,column=1, sticky='n')
+txt2.grid(row=2,column=0, sticky='we')
+frame.grid(row=2,column=1, sticky='n')
 btn1.pack(fill=tk.X)
 btn2.pack(fill=tk.X)
 btn3.pack(fill=tk.X)
+labelIPA = tk.Label(root, font=('Arial', 16))
+labelIPA.grid(row=1,column=0)
 txt1.insert(tk.END,s_origin)
 
 tk.mainloop()
